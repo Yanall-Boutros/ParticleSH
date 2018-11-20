84a5d4195e4b4df95fdeb23dbc1d542076d8d31d
Yanall-Boutros <yboutros@ucsc.edu>
2018-11-19 20:24:58 -0800

    Updated Histograms, fixed logic in function is_massless_And_isolated

--- jet_prac/ttbar/run_ttbar.py
+++ jet_prac/ttbar/run_ttbar.py
@@ -20,7 +20,7 @@ unclustered_particles = list()
 def is_massless_and_isolated(jet):
    # Returns true if a jet has nconsts = 1 and has a pdgid equal to that
    # of a photon or a gluon, (todo: When to also discard muons)
-   if len(jet.constituents_array()) == 0: 
+   if len(jet.constituents_array()) == 1: 
       if np.abs(jet.info['pdgid']) == 21 or np.abs(jet.info['pdgid']) == 22:
          return True
       return np.abs(jet.info['pdgid']) == 13 and 2*jet.mass/jet.pt > 0.4
