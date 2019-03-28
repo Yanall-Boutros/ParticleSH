d918f58f8711c73bc769632162fcb0cbc8e59a1d
Yanall-Boutros <yboutros@ucsc.edu>
2019-03-28 11:34:58 -0700

    Bug fixes

--- 2d_hists/ttbar/energy.py
+++ 2d_hists/ttbar/energy.py
@@ -9,16 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pyjet import cluster
 from matplotlib.colors import ListedColormap
-def print_debug_data(dd):
-    for tup in dd:
-        j = tup[1]
-        print("Mass = ", j[0], "\tpT = ", j[3], "\tEffR = ", j[5])
-def allequs(jet):
-    mag = (jet.px**2 + jet.py**2 + jet.pz**2)**0.5
-    eng = jet.constituents_array(ep=True)[0][0]
-    if mag == eng and eng == jet.e:
-        return True
-    return False
+id = 'pdgid'
 # -----------------------------------------------------------------------
 # Load Colormap info
 # -----------------------------------------------------------------------
@@ -43,35 +34,38 @@ unclustered_particles = list()
 # Generate Jets and Histogram Data
 # -----------------------------------------------------------------------
 def is_massless_or_isolated(jet):
-   # Returns true if a jet has nconsts = 1 and has a pdgid equal to that
-   # of a photon or a gluon
-   if len(jet.constituents_array()) == 1: 
-      if np.abs(jet.info['pdgid']) == 21 or np.abs(jet.info['pdgid']) == 22:
-         return True
-      # if a muon is outside of the radius of the jet, discard it
-      if np.abs(jet.info['pdgid']) == 13:
-         if 2*jet.mass/jet.pt > 0.4: return True
-   # Remove Jets with too high an eta
-   if np.abs(jet.eta) > 5.0:
-      return True
-   # Remove any jets less than an arbitrary near zero mass
-   if jet.mass < 0.4:
-      return True
-   return False
-
+    # Returns true if a jet has nconsts = 1 and has a pdgid equal to that
+    # of a photon or a gluon
+    if len(jet.constituents_array()) == 1: 
+        if np.abs(jet.info[id]) == 21 or np.abs(jet.info[id]) == 22:
+            return True
+        # if a muon is outside of the radius of the jet, discard it
+        if np.abs(jet.info['pdgid']) == 13:
+            if 2*jet.mass/jet.pt > 0.4: return True
+     # Remove Jets with too high an eta
+    if np.abs(jet.eta) > 5.0:
+        return True
+    # Remove any jets less than an arbitrary near zero mass
+    if jet.mass < 0.4:
+        return True
+    return False
 def return_particle_data(jet):
     # return the array containing all the eta, phi, and energies of the
     # particles in a jets constituent array
     vals = []
-    has_eta_phi = jet.constituents_array(ep=True)
-    has_e = jet.constituents_array(ep=True)
-    e = []
+    has_eta_phi = jet.constituents_array()
     eta = []
     phi = []
+    m = []
+    pt = []
     for i in range(len(has_eta_phi)):
-        e.append(has_e[i][0])
-        eta.append(has_eta_phi[i][2])
+        pt.append(has_eta_phi[i][0])
+        eta.append(has_eta_phi[i][1])
         phi.append(has_eta_phi[i][2])
+        m.append(has_eta_phi[i][3])
+    m = np.array(m)
+    pt = np.array(pt)
+    e = (pt**2 + m**2)**0.5
     return [eta, phi, e]
 debug_data = []
 leading_data = []
@@ -86,98 +80,32 @@ discarded_data = []
 # -----------------------------------------------------------------------
 # Main Loop for Storing Jet Data
 # -----------------------------------------------------------------------
-for event in pythia(events=10000):
-   vectors = event.all(selection)
-   sequence = cluster(vectors, R=0.4, p=-1, ep=True)
-   jets = sequence.inclusive_jets()
-   unclustered_particles.append(sequence.unclustered_particles())
-   for i, jet in enumerate(jets):
-      data = (
-              jet.eta, jet.phi, jet.e
-             )
-      part_data = return_particle_data(jet)
-
-      if is_massless_or_isolated(jet):
-         discarded_data.append(jet)
-      else:
-         # Append Leading Jet information
-         if i == 0:
-            leading_data.append(data)
-            leading_particle_eta.extend(part_data[0])
-            leading_particle_phi.extend(part_data[1])
-            leading_particle_energy.extend(part_data[2])
-         jets_data.append(data)
-         jets_particle_eta.extend(part_data[0])
-         jets_particle_phi.extend(part_data[1])
-         jets_particle_energy.extend(part_data[2])
-# -----------------------------------------------------------------------
-# Plot 3D-histograms
-# -----------------------------------------------------------------------
-leading_xdata = np.array(leading_data[0]) # Eta
-leading_ydata = np.array(leading_data[1]) # Phi
-leading_zdata = np.array(leading_data[2]) # Energy weight
-
-jet_xdata = np.array(jets_data[0])
-jet_ydata = np.array(jets_data[1])
-jet_zdata = np.array(jets_data[2])
-
-plt.figure()
-plt.hist2d(leading_particle_eta, leading_particle_phi,
-        weights=leading_particle_energy, range=[(-5,5),(-1*np.pi,np.pi)],
-        bins=(10,10), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Leading_Particles_TTbar.pdf")
-
-plt.figure()
-plt.hist2d(jets_particle_eta, jets_particle_phi,
-        weights=jets_particle_energy, range=[(-5,5),(-1*np.pi,np.pi)],
-        bins=(10,10), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Jets_Particles_TTbar.pdf")
-
-plt.figure()
-plt.hist2d(leading_xdata, leading_ydata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(5, 5), cmap='cubehelix')
-#cax = ax.imshow((leading_xdata, leading_ydata), cmap=
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Leading_Jet_TTbar.pdf")
-
-plt.figure()
-plt.hist2d(jet_xdata, jet_ydata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(5, 5), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Aggregate_Jet_TTbar.pdf")
-
-plt.figure()
-plt.hist2d(jet_xdata, jet_ydata, weights=jet_zdata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(5, 5), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Energy_Weighted_Aggregate_Jet_TTbar.pdf")
-
-plt.figure()
-plt.hist2d(leading_xdata, leading_ydata, weights=leading_zdata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(5, 5), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$T\overline{T}$")
-plt.colorbar()
-plt.savefig("Energy_Weighted_Leading_Jet_TTbar.pdf")
+a = 0
+for event in pythia(events=10):
+    vectors = event.all(selection)
+    sequence = cluster(vectors, R=0.4, p=-1, ep=True)
+    jets = sequence.inclusive_jets()
+    unclustered_particles.append(sequence.unclustered_particles())
+    part_data = []
+    for i, jet in enumerate(jets):
+        data = (
+                jet.eta, jet.phi, jet.e
+               )
+        part_data = return_particle_data(jet)
+        if is_massless_or_isolated(jet):
+            discarded_data.append(jet)
+        else:
+            jets_particle_eta.extend(part_data[0])
+            jets_particle_phi.extend(part_data[1])
+            jets_particle_energy.extend(part_data[2])
+    plt.figure()
+    plt.hist2d(jets_particle_eta, jets_particle_phi,
+           weights=jets_particle_energy,
+           range=[(-5,5),(-1*np.pi,np.pi)],
+           bins=(5,5), cmap='cubehelix')
+    plt.xlabel("$\eta$")
+    plt.ylabel("$\phi$")
+    plt.title("$T\overline{T}$")
+    plt.colorbar()
+    plt.savefig("Jets_Particles_TTbar"+str(a)+".png")
+    a += 1
--- 2d_hists/zz/energy.py
+++ 2d_hists/zz/energy.py
@@ -9,16 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pyjet import cluster
 from matplotlib.colors import ListedColormap
-def print_debug_data(dd):
-    for tup in dd:
-        j = tup[1]
-        print("Mass = ", j[0], "\tpT = ", j[3], "\tEffR = ", j[5])
-def allequs(jet):
-    mag = (jet.px**2 + jet.py**2 + jet.pz**2)**0.5
-    eng = jet.constituents_array(ep=True)[0][0]
-    if mag == eng and eng == jet.e:
-        return True
-    return False
+id = 'pdgid'
 # -----------------------------------------------------------------------
 # Load Colormap info
 # -----------------------------------------------------------------------
@@ -43,69 +34,78 @@ unclustered_particles = list()
 # Generate Jets and Histogram Data
 # -----------------------------------------------------------------------
 def is_massless_or_isolated(jet):
-   # Returns true if a jet has nconsts = 1 and has a pdgid equal to that
-   # of a photon or a gluon
-   if len(jet.constituents_array()) == 1: 
-      if np.abs(jet.info['pdgid']) == 21 or np.abs(jet.info['pdgid']) == 22:
-         return True
-      # if a muon is outside of the radius of the jet, discard it
-      if np.abs(jet.info['pdgid']) == 13:
-         if 2*jet.mass/jet.pt > 0.4: return True
-   # Remove Jets with too high an eta
-   if np.abs(jet.eta) > 5.0:
-      return True
-   # Remove any jets less than an arbitrary near zero mass
-   if jet.mass < 0.4:
-      return True
-   return False
+    # Returns true if a jet has nconsts = 1 and has a pdgid equal to that
+    # of a photon or a gluon
+    if len(jet.constituents_array()) == 1: 
+        if np.abs(jet.info[id]) == 21 or np.abs(jet.info[id]) == 22:
+            return True
+        # if a muon is outside of the radius of the jet, discard it
+        if np.abs(jet.info['pdgid']) == 13:
+            if 2*jet.mass/jet.pt > 0.4: return True
+     # Remove Jets with too high an eta
+    if np.abs(jet.eta) > 5.0:
+        return True
+    # Remove any jets less than an arbitrary near zero mass
+    if jet.mass < 0.4:
+        return True
+    return False
+def return_particle_data(jet):
+    # return the array containing all the eta, phi, and energies of the
+    # particles in a jets constituent array
+    vals = []
+    has_eta_phi = jet.constituents_array()
+    eta = []
+    phi = []
+    m = []
+    pt = []
+    for i in range(len(has_eta_phi)):
+        pt.append(has_eta_phi[i][0])
+        eta.append(has_eta_phi[i][1])
+        phi.append(has_eta_phi[i][2])
+        m.append(has_eta_phi[i][3])
+    m = np.array(m)
+    pt = np.array(pt)
+    e = (pt**2 + m**2)**0.5
+    return [eta, phi, e]
 debug_data = []
 leading_data = []
+leading_particle_eta = []
+leading_particle_phi = []
+leading_particle_energy = []
 jets_data = []
+jets_particle_eta = []
+jets_particle_phi = []
+jets_particle_energy = []
 discarded_data = [] 
 # -----------------------------------------------------------------------
 # Main Loop for Storing Jet Data
 # -----------------------------------------------------------------------
-for event in pythia(events=10000):
-   vectors = event.all(selection)
-   sequence = cluster(vectors, R=0.4, p=-1, ep=True)
-   jets = sequence.inclusive_jets()
-   unclustered_particles.append(sequence.unclustered_particles())
-   for i, jet in enumerate(jets):
-      data = (
-              jet.eta, jet.phi, jet.e,
-             )
-      if is_massless_or_isolated(jet):
-         discarded_data.append(jet)
-      else:
-         # Append Leading Jet information
-         if i == 0:
-            leading_data.append(data)
-         jets_data.append(data)
-# -----------------------------------------------------------------------
-# Plot 3D-histograms
-# -----------------------------------------------------------------------
-leading_xdata = np.array(leading_data[0])
-leading_ydata = np.array(leading_data[1])
-leading_zdata = np.array(leading_data[2])
-jet_xdata = np.array(jets_data[0])
-jet_ydata = np.array(jets_data[1])
-jet_zdata = np.array(jets_data[2])
-plt.figure()
-plt.hist2d(leading_xdata, leading_ydata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(50, 50), cmap='cubehelix')
-#cax = ax.imshow((leading_xdata, leading_ydata), cmap=
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$ZZ$")
-plt.colorbar()
-plt.savefig("LeadingZZ.pdf")
-plt.figure()
-plt.hist2d(jet_xdata, jet_ydata,
-    range=[(-5,5),(-1*np.pi, np.pi)],
-    bins=(50, 50), cmap='cubehelix')
-plt.xlabel("$\eta$")
-plt.ylabel("$\phi$")
-plt.title("$ZZ$")
-plt.colorbar()
-plt.savefig("AggregateZZ.pdf")
+a = 0
+for event in pythia(events=10):
+    vectors = event.all(selection)
+    sequence = cluster(vectors, R=0.4, p=-1, ep=True)
+    jets = sequence.inclusive_jets()
+    unclustered_particles.append(sequence.unclustered_particles())
+    part_data = []
+    for i, jet in enumerate(jets):
+        data = (
+                jet.eta, jet.phi, jet.e
+               )
+        part_data = return_particle_data(jet)
+        if is_massless_or_isolated(jet):
+            discarded_data.append(jet)
+        else:
+            jets_particle_eta.extend(part_data[0])
+            jets_particle_phi.extend(part_data[1])
+            jets_particle_energy.extend(part_data[2])
+    plt.figure()
+    plt.hist2d(jets_particle_eta, jets_particle_phi,
+           weights=jets_particle_energy,
+           range=[(-5,5),(-1*np.pi,np.pi)],
+           bins=(5,5), cmap='cubehelix')
+    plt.xlabel("$\eta$")
+    plt.ylabel("$\phi$")
+    plt.title("$ZZ$")
+    plt.colorbar()
+    plt.savefig("Jets_Particles_ZZ"+str(a)+".png")
+    a += 1
